2ded75a52f1ed41487179c6629ae12737eb92a9b
Chris <chrislo.7@live.com>
2018-09-14 20:53:47 +0000

    fixed empty cart bug

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,6 @@
 class Admin::DashboardController < ApplicationController
   name = ENV["ADMIN_USERNAME"]
   password = ENV["ADMIN_PASSWORD"]
-
   http_basic_authenticate_with name: name, password: password
 
   def show
