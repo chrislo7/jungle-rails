a048e00001073e2b8cbe1f76c874702fbbf69f99
Chris <chrislo.7@live.com>
2018-09-15 02:23:44 +0000

    hide submit when un-logged in users are viewing product details

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,29 @@
 class ReviewsController < ApplicationController
+
+  before_action :verify_user, only :create
+
    def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(rating: params[:review][:rating], description: params[:review][:description])
     @review.user = current_user
-     if @review.save!
+     if @review.save
       redirect_to product_path product_id
     else
       redirect_to root_path
     end
    end
+
    private
+
   def product_id
     params[:product_id]
   end
+
+   def verify_user
+    # binding.pry
+    if !current_user
+      redirect_to login_path
+    end
+  end
+
 end
